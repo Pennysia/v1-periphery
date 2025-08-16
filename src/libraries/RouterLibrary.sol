946dba465a99a5602b9b7b9938dca1d124b403bd
Settee <ake14243@gmail.com>
2025-08-16 20:10:26 +0700

    forge fmt

--- src/libraries/RouterLibrary.sol
+++ src/libraries/RouterLibrary.sol
@@ -13,50 +13,36 @@ library RouterLibrary {
         require(tokenA < tokenB, tokensNotSorted());
     }
 
-    function sortTokens(
-        address tokenA,
-        address tokenB
-    ) internal pure returns (address token0, address token1) {
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
         require(tokenA != tokenB, identicalTokens());
-        (token0, token1) = tokenA < tokenB
-            ? (tokenA, tokenB)
-            : (tokenB, tokenA);
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
     }
 
-    function computePairId(
-        address token0,
-        address token1
-    ) internal pure returns (uint256 pairId) {
+    function computePairId(address token0, address token1) internal pure returns (uint256 pairId) {
         pairId = uint256(keccak256(abi.encodePacked(token0, token1)));
     }
 
-    function getReserves(
-        address market,
-        address tokenA,
-        address tokenB
-    ) internal view returns (uint256 reserveA, uint256 reserveB) {
+    function getReserves(address market, address tokenA, address tokenB)
+        internal
+        view
+        returns (uint256 reserveA, uint256 reserveB)
+    {
         (address token0, address token1) = sortTokens(tokenA, tokenB);
-        (
-            uint256 reserve0Long,
-            uint256 reserve0Short,
-            uint256 reserve1Long,
-            uint256 reserve1Short
-        ) = getDirectionalBalances(market, token0, token1);
+        (uint256 reserve0Long, uint256 reserve0Short, uint256 reserve1Long, uint256 reserve1Short) =
+            getDirectionalBalances(market, token0, token1);
 
         uint256 reserve0 = reserve0Long + reserve0Short;
         uint256 reserve1 = reserve1Long + reserve1Short;
-        (reserveA, reserveB) = tokenA == token0
-            ? (reserve0, reserve1)
-            : (reserve1, reserve0);
+        (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
 
     // --------- Get Amounts ---------
 
-    function getAmountOut(
-        uint256 amountIn,
-        uint256 reserveIn,
-        uint256 reserveOut
-    ) internal pure returns (uint256 amountOut) {
+    function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut)
+        internal
+        pure
+        returns (uint256 amountOut)
+    {
         require(amountIn > 0, insufficientAmounts());
         require(reserveIn > 0 && reserveOut > 0, pairZeroBalance());
         // Calculate amountOut using constant product formula: x * y = k
@@ -68,11 +54,11 @@ library RouterLibrary {
         amountOut -= feeAmountOut;
     }
 
-    function getAmountIn(
-        uint256 amountOut,
-        uint256 reserveIn,
-        uint256 reserveOut
-    ) internal pure returns (uint256 amountIn) {
+    function getAmountIn(uint256 amountOut, uint256 reserveIn, uint256 reserveOut)
+        internal
+        pure
+        returns (uint256 amountIn)
+    {
         require(amountOut > 0, insufficientAmounts());
         require(reserveIn > 0 && reserveOut > 0, pairZeroBalance());
         uint256 amountOutWithFee = ((amountOut * 1003 + 999) / 1000); // divUp equivalent
@@ -81,24 +67,13 @@ library RouterLibrary {
         amountIn = newReserveIn - reserveIn;
     }
 
-    function getDirectionalBalances(
-        address market,
-        address token0,
-        address token1
-    )
+    function getDirectionalBalances(address market, address token0, address token1)
         internal
         view
-        returns (
-            uint256 reserve0Long,
-            uint256 reserve0Short,
-            uint256 reserve1Long,
-            uint256 reserve1Short
-        )
+        returns (uint256 reserve0Long, uint256 reserve0Short, uint256 reserve1Long, uint256 reserve1Short)
     {
         onlySorted(token0, token1);
-        (reserve0Long, reserve0Short, reserve1Long, reserve1Short) = IMarket(
-            market
-        ).getReserves(token0, token1);
+        (reserve0Long, reserve0Short, reserve1Long, reserve1Short) = IMarket(market).getReserves(token0, token1);
     }
 
     function quoteLiquidity(
@@ -109,11 +84,7 @@ library RouterLibrary {
         uint256 amountShort0,
         uint256 amountLong1,
         uint256 amountShort1
-    )
-        internal
-        view
-        returns (uint256 longX, uint256 shortX, uint256 longY, uint256 shortY)
-    {
+    ) internal view returns (uint256 longX, uint256 shortX, uint256 longY, uint256 shortY) {
         //TODO: complete this, must check sorting of token0 and token1
     }
 }
